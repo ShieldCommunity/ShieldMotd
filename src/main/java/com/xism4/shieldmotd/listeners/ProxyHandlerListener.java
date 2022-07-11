a0d89d60ce2dd42ba95e58bc90a25aa458e33491
jonakls <asmot54@hotmail.com>
2022-07-10 22:48:54 -0500

    feat: fixed bug that return value list in string object, changed for return real string value

--- src/main/java/com/xism4/shieldmotd/listeners/ProxyHandlerListener.java
+++ src/main/java/com/xism4/shieldmotd/listeners/ProxyHandlerListener.java
@@ -1,22 +1,26 @@
 package com.xism4.shieldmotd.listeners;
 
+import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 
 import com.xism4.shieldmotd.ShieldMotd;
+import com.xism4.shieldmotd.manager.ChannelHandlerManager;
+import com.xism4.shieldmotd.manager.ConfigurationManager;
 import com.xism4.shieldmotd.utils.FastMotdException;
 import io.netty.channel.ChannelHandlerContext;
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.protocol.BadPacketException;
 import net.md_5.bungee.protocol.OverflowPacketException;
 
+import static com.xism4.shieldmotd.utils.TextUtils.toComponent;
+
 public class ProxyHandlerListener implements Listener {
 
-    private ShieldMotd core;
+    private final ShieldMotd core;
     private static long lastInitialException;
     private Throwable cause;
 
@@ -26,24 +30,32 @@ public class ProxyHandlerListener implements Listener {
 
     @EventHandler(priority = 64)
     public void proxyHandler(ProxyPingEvent event) {
+
+        ConfigurationManager configurationManager = core.getConfigurationManager();
+        ChannelHandlerManager channelHandlerManager = core.getChannelHandlerManager();
         ServerPing pingHandler = event.getResponse();
         String address = event.getConnection().getAddress().getAddress().getHostAddress();
 
         if (pingHandler == null) {
-            core.getChannelHandlerManager().closeChannel((ChannelHandlerContext) this, address);
+            channelHandlerManager.closeChannel((ChannelHandlerContext) this, address);
             core.getLogger().warning("An handshake was null and cancelled");
             return;
         }
 
         if (cause instanceof FastMotdException || cause instanceof BadPacketException || cause instanceof OverflowPacketException) {
-            core.getChannelHandlerManager().closeChannel((ChannelHandlerContext) this, address);
-            core.getInstance().getLogger().log(Level.INFO,
+            channelHandlerManager.closeChannel((ChannelHandlerContext) this, address);
+            core.getLogger().log(Level.INFO,
                     "The connection -> {0} rate-limited bad-joins, we suggest blacklist", address);
             return;
         }
 
-        pingHandler.setDescriptionComponent(
-                new TextComponent(ChatColor.translateAlternateColorCodes('&',
-                        core.getConfigurationManager().getStringList("motd.lines"))));
+        //TODO: Temporary fix for the motd bug, will be removed in the future @Jonakls
+        List<String> motd = configurationManager.getConfig().getStringList("motd.lines");
+        Random random = new Random();
+        String motdLine = motd.get(random.nextInt(motd.size()));
+
+        pingHandler.setDescriptionComponent(toComponent(motdLine));
+
+
     }
 }
