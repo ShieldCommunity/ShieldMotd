c5b3c7da8c83583f9297130cb421a8d2cd44c5d3
jonakls <asmot54@hotmail.com>
2022-07-10 22:47:44 -0500

    feat: make a struct for motd manager

---
+++ src/main/java/com/xism4/shieldmotd/manager/MotdManager.java
@@ -0,0 +1,30 @@
+package com.xism4.shieldmotd.manager;
+
+import com.xism4.shieldmotd.ShieldMotd;
+
+import java.util.Random;
+
+public class MotdManager {
+
+    private final ShieldMotd plugin;
+    private Random random;
+
+    public MotdManager(ShieldMotd plugin) {
+        //TODO: implement system for motd here, create individual system for make a correct structure for motd
+        this.plugin = plugin;
+    }
+
+    public void setupMotd() {
+        //TODO: setup all motds list from config file
+    }
+
+    public void makeMotd() {
+        //TODO: make one motd list and split lines with \n value from config file
+    }
+
+    public String getMotd() {
+        //TODO: return single motd and processed with random selection
+        return "JA";
+    }
+
+}
