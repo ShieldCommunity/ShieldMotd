4646c148ea0adec913c74cca18910530786d3bf7
4drian3d <adriangonzalesval@gmail.com>
2022-07-11 19:10:49 +0000

    fix: Unable to execute shieldmotd on console

--- src/main/java/com/xism4/shieldmotd/command/ShieldMotdCommand.java
+++ src/main/java/com/xism4/shieldmotd/command/ShieldMotdCommand.java
@@ -4,7 +4,6 @@ import com.xism4.shieldmotd.ShieldMotd;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class ShieldMotdCommand extends Command {
@@ -12,30 +11,27 @@ public class ShieldMotdCommand extends Command {
     private final ShieldMotd core;
 
     public ShieldMotdCommand(ShieldMotd core) {
-        super("ShieldMotd");
+        super("shieldmotd", "shieldmotd.command");
         this.core = core;
     }
 
+    @Override
     public void execute(CommandSender sender, String[] args) {
-        if (sender instanceof ProxiedPlayer) {
-            final ProxiedPlayer player = (ProxiedPlayer) sender;
+        if (args.length == 0) {
+            sender.sendMessage(new ComponentBuilder("ShieldMotd - simple & lightweight motd plugin by xIsm4")
+                    .color(ChatColor.YELLOW).create());
+            return;
+        }
 
-            if (args.length == 0) {
-                player.sendMessage(new ComponentBuilder("ShieldMotd - simple & lightweight motd plugin by xIsm4")
-                        .color(ChatColor.YELLOW).create());
-                return;
-            }
+        if ("reload".equalsIgnoreCase(args[0])) {
+            core.getConfigurationManager().reload();
+            core.getMotdManager().setupMotd();
+            sender.sendMessage(new ComponentBuilder("ShieldMotd - has been reload successfully")
+                    .color(ChatColor.YELLOW).create());
 
-            if ("reload".equalsIgnoreCase(args[0])) {
-                core.getConfigurationManager().reload();
-                core.getMotdManager().setupMotd();
-                player.sendMessage(new ComponentBuilder("ShieldMotd - has been reload successfully")
-                        .color(ChatColor.YELLOW).create());
-
-            } else {
-                player.sendMessage(new ComponentBuilder("ShieldMotd - The command does not exist")
-                        .color(ChatColor.RED).create());
-            }
+        } else {
+            sender.sendMessage(new ComponentBuilder("ShieldMotd - The command does not exist")
+                    .color(ChatColor.RED).create());
         }
     }
 }
