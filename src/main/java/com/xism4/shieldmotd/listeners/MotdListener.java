7960c77c9217d3e7a73830f8cb7a335a4204ffec
xIsm4 <soportexism4@gmail.com>
2023-06-11 18:15:04 +0200

    Close with channelwrapper if downsstream connection exists (BoomEaro)

--- src/main/java/com/xism4/shieldmotd/listeners/MotdListener.java
+++ src/main/java/com/xism4/shieldmotd/listeners/MotdListener.java
@@ -1,7 +1,9 @@
 package com.xism4.shieldmotd.listeners;
 
 import com.xism4.shieldmotd.ShieldMotd;
+import com.xism4.shieldmotd.manager.ByteBuff;
 import com.xism4.shieldmotd.manager.ConfigurationHandler;
+import com.xism4.shieldmotd.manager.IChannelWrapper;
 import io.netty.channel.ChannelHandlerContext;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.event.ProxyPingEvent;
@@ -16,13 +18,13 @@ public class MotdListener implements Listener {
     }
 
     @EventHandler(priority = 64)
-    public void proxyHandler(ProxyPingEvent event, ChannelHandlerContext ctx) {
+    public void proxyHandler(ProxyPingEvent event, IChannelWrapper channelWrapper) {
 
         ServerPing pingHandler = event.getResponse();
 
         if (pingHandler == null
                 && ConfigurationHandler.IMP.MOTD.CLOSE_ON_INVALID_PING) {
-            ctx.channel().close();
+            channelWrapper.close();
             core.getLogger().warning(
                     "Invalid ping response"
             );
