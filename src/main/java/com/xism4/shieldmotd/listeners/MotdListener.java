62eb5d6a3f83888e1d72c219ea29af0a176e92f7
Ismael Hanbel <soportexism4@gmail.com>
2023-10-11 01:57:56 +0200

    Not send message to console/client, directly disconect.

--- src/main/java/com/xism4/shieldmotd/listeners/MotdListener.java
+++ src/main/java/com/xism4/shieldmotd/listeners/MotdListener.java
@@ -3,7 +3,6 @@ package com.xism4.shieldmotd.listeners;
 import com.xism4.shieldmotd.ShieldMotd;
 import com.xism4.shieldmotd.manager.MotdManager;
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -22,10 +21,7 @@ public class MotdListener implements Listener {
         ServerPing pingHandler = event.getResponse();
 
         if (pingHandler == null) {
-               event.getConnection().disconnect(new TextComponent(
-                       "Invalid ping?")
-               );
-            core.getLogger().warning("Invalid ping response");
+               event.getConnection().disconnect();
             return;
         }
 
