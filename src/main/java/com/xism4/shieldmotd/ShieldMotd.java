7cc106f067a15538bd346896f6a1f02a6478d54d
Ismael Hanbel <soportexism4@gmail.com>
2024-06-19 06:34:32 +0200

    Advise the proxy native-feature if is detected

--- src/main/java/com/xism4/shieldmotd/ShieldMotd.java
+++ src/main/java/com/xism4/shieldmotd/ShieldMotd.java
@@ -9,23 +9,35 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
 
+import java.util.logging.Level;
+
 public final class ShieldMotd extends Plugin {
 
     private MotdManager motdManager;
+    private ProxyServer proxyServer;
 
     @Override
     public void onEnable() {
         ShieldMotdConfig.IMP.reload();
+
+        if(proxyServer.getName().contains("NullCordX")) {
+            this.getLogger().log(
+                    Level.WARNING,
+                    "NullCordX is detected and is highly recommend to use native-built feature"
+            );
+        }
+
         this.motdManager = new MotdManager();
+
         PluginManager pluginManager = getProxy().getPluginManager();
+
         pluginManager.registerCommand(
                 this, new ShieldMotdCommand(this)
         );
+
         pluginManager.registerListener(
                 this, new MotdListener(this)
         );
-
-        getLogger().info("ShieldMotd has been loaded successfully, running on " + ProxyServer.getInstance().getName());
     }
 
     @Override
